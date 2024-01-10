c9062a422c1f128764199026ee7ba7eda4bd6432
Chrystyan <24556350+ciurescuraul@users.noreply.github.com>
2024-01-10 03:42:32 +0200

    feat(kafka-producer): Handle Exception in Rest Controller

--- src/main/java/ro/cyberdev/products/rest/ProductController.java
+++ src/main/java/ro/cyberdev/products/rest/ProductController.java
@@ -1,5 +1,7 @@
 package ro.cyberdev.products.rest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,10 +10,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ro.cyberdev.products.service.ProductService;
 
+import java.util.Date;
+
 @RestController
 @RequestMapping("/products") // http://localhost:<port>/products
 public class ProductController {
-
+    private final Logger log = LoggerFactory.getLogger(ProductController.class);
     private final ProductService productService;
 
     public ProductController(ProductService productService) {
@@ -19,9 +23,17 @@ public class ProductController {
     }
 
     @PostMapping
-    public ResponseEntity<String> createProduct(@RequestBody CreateProductRestModel product) {
+    public ResponseEntity<Object> createProduct(@RequestBody CreateProductRestModel product) {
 
-        String productId = productService.createProduct(product);
+        String productId;
+        try {
+            productId = productService.createProduct(product);
+        } catch (Exception e) {
+//            e.printStackTrace();
+            log.error(e.getMessage(),e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ErrorMessage(new Date(), e.getMessage(), "/products"));
+        }
 
         return ResponseEntity.status(HttpStatus.CREATED).body(productId);
     }
